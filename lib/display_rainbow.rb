fccd2f55b783ec3ff173fc2bff37ed9f7857404f
Kenneth Lehr <kenneth.c.lehr@gmail.com>
2018-01-03 03:07:51 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,11 @@
 # Write your #display_rainbow method here
 
-def distplay_rainbow()
+def display_rainbow(colors)
+  put "R: #{colors[0]}"
+  put "R: #{colors[1]}"
+  put "R: #{colors[2]}"
+  put "R: #{colors[3]}"
+  put "R: #{colors[4]}"
+  put "R: #{colors[5]}"
+  put "R: #{colors[6]}"
+end 
